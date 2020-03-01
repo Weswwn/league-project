2013f0b6eec7c16117ebdca0d3c90a6771797b3d
Wes <wesley.qb.wen@gmail.com>
2020-03-01 13:06:35 -0800

    Initial setup files and code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  componentDidMount() {
+    
+  }
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+
+export default App;
